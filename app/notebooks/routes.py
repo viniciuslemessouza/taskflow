e425e63d6f0ee139f6f6e725e37954fdf1114a87
Vinícius Lemes de Souza <viniciuslemes314@gmail.com>
2026-06-15 16:18:25 -0300

    chore: change dummy data for database

--- app/notebooks/routes.py
+++ app/notebooks/routes.py
@@ -1,15 +1,25 @@
-from flask import Blueprint, render_template
-from app.notebooks.forms import NewNotebookForm, NewTaskForm
 from app.notebooks.models import Notebook, Task
+from flask_login import current_user, login_required
+from flask import Blueprint, render_template, request, abort
+from app.notebooks.forms import NewNotebookForm, NewTaskForm
 
 notebooks = Blueprint('notebooks', __name__)
 
 @notebooks.route('/notebooks')
+@login_required
 def get_notebooks():
     form = NewNotebookForm()
-    return render_template('notebooks/notebooks.html', form=form)
+    page = request.args.get('page', 1, type=int)
+    user_notebooks = Notebook.query.filter_by(user_id=current_user.id).order_by(Notebook.id.desc()).paginate(page=page, per_page=12)
+    return render_template('notebooks/notebooks.html', form=form, notebooks=user_notebooks)
 
 @notebooks.route('/notebook/<notebook_id>')
+@login_required
 def get_notebook(notebook_id):
     form = NewTaskForm()
-    return render_template('notebooks/notebook.html', form=form)
+    user_notebook = Notebook.query.get_or_404(notebook_id)
+    if user_notebook.user != current_user:
+        abort(403)
+
+    tasks = Task.query.filter_by(notebook_id=user_notebook.id).order_by(Task.id.desc()).all()
+    return render_template('notebooks/notebook.html', form=form, tasks=tasks, notebook_title=user_notebook.title)
--- create_db.py
+++ create_db.py
@@ -1,4 +1,3 @@
-import secrets
 from flask import Flask
 from config import Config
 from app import db
@@ -11,31 +10,14 @@ from app.notebooks.models import Notebook, Task
 
 db.init_app(app)
 
-def create_user():
-    user1 = User(fullname='Vinicius Lemes de Souza', email='viniciuslemes314@gmail.com', password=secrets.token_hex(8),
-                 terms_agreement=True, profile_picture='default.png')
-    user2 = User(fullname='Debora Barbara Viana Alves da Silva', email='deborabvas@gmail.com',
-                 password=secrets.token_hex(8), terms_agreement=True, profile_picture='default.png')
-    user3 = User(fullname='Vinicius Lemes de Souza', email='viniciuslemes31415@gmail.com',
-                 password=secrets.token_hex(8), terms_agreement=True, profile_picture='default.png')
-    db.session.add(user1)
-    db.session.add(user2)
-    db.session.add(user3)
-
-def create_notebook(index, user_id):
-    notebook = Notebook(title=f"Notebook {index}", user_id=user_id)
-    db.session.add(notebook)
-
-def create_task(index, notebook_id):
-    task = Task(content=f"Task {index}", notebook_id=notebook_id)
-    db.session.add(task)
-
 with app.app_context():
     db.create_all()
-    create_user()
     for user_index in range(1, 4):
         for notebook_index in range(1, 101):
-            create_notebook(notebook_index, user_index)
+            notebook = Notebook(title=f"Notebook {notebook_index}", user_id=user_index)
+            db.session.add(notebook)
+            db.session.flush()
             for task_index in range(1, 101):
-                create_task(task_index, notebook_index)
+                task = Task(content=f"Task {task_index}", notebook_id=notebook.id)
+                db.session.add(task)
     db.session.commit()
